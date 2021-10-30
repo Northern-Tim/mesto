3d5387e0a322c2163eeeb068b1d84ce70f42a8b5
Artem Bondarenko <bondaren.ar@yandex.ru>
2021-10-30 14:28:01 +0300

    Написать JS и изменить значения текста

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+let changeButton = document.querySelector('.menu__user-info-changes-button');
+let popup = document.querySelector('.popup');
+let userName = document.querySelector('.menu__user-name');
+let userWorking = document.querySelector('.menu__user-working');
+let closeButton = document.querySelector('.popup__close-btn');
+let saveButton = document.querySelector('.popup__save-btn');
+let formElement = document.querySelector('.popup__form');
+let inputUserName = document.querySelector('.popup__input-name');
+let inputUserWork = document.querySelector('.popup__input-work');
+
+
+function openPopup() {
+  popup.classList.add("popup_opened");
+  inputUserName.value = userName.textContent;
+  inputUserWork.value = userWorking.textContent;
+};
+changeButton.addEventListener('click', openPopup); 
+
+
+function closePopup() {
+  popup.classList.remove("popup_opened");
+};
+closeButton.addEventListener('click', closePopup); 
+
+
+function formSubmitHandler (evt) {
+  evt.preventDefault(); 
+  let UserNameVal = inputUserName.value;
+  let UserWorkVal = inputUserWork.value;
+  userName.textContent = inputUserName.value;
+  userWorking.textContent = inputUserWork.value;
+}
+formElement.addEventListener('submit', formSubmitHandler); 
+saveButton.addEventListener('click', closePopup); 
