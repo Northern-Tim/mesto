d4a7710531068de448bcfc171a451c3a25c0851a
Artem Bondarenko <bondaren.ar@yandex.ru>
2021-11-02 18:29:08 +0300

    Внести правки ревьюера, чтобы сравнить с примером

--- scripts/index.js
+++ scripts/index.js
@@ -3,30 +3,27 @@ let popup = document.querySelector('.popup');
 let userName = document.querySelector('.menu__user-name');
 let userWorking = document.querySelector('.menu__user-working');
 let closeButton = document.querySelector('.popup__close-btn');
-let saveButton = document.querySelector('.popup__save-btn');
 let formElement = document.querySelector('.popup__form');
-let inputUserName = document.querySelector('.popup__input-name');
-let inputUserWork = document.querySelector('.popup__input-work');
+let inputUserName = document.querySelector('.popup__input_value_name');
+let inputUserWork = document.querySelector('.popup__input_value_work');
 
 function openPopup() {
-  popup.classList.add("popup_opened");
   inputUserName.value = userName.textContent;
   inputUserWork.value = userWorking.textContent;
+  popup.classList.add('popup_opened');
 };
 
 function closePopup() {
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 };
 
-function formSubmitHandler (evt) {
-  evt.preventDefault(); 
-  let UserNameVal = inputUserName.value;
-  let UserWorkVal = inputUserWork.value;
+function formSubmitHandler(evt) {
+  evt.preventDefault();
   userName.textContent = inputUserName.value;
   userWorking.textContent = inputUserWork.value;
+  closePopup();
 }
 
-changeButton.addEventListener('click', openPopup); 
-closeButton.addEventListener('click', closePopup); 
-formElement.addEventListener('submit', formSubmitHandler); 
-saveButton.addEventListener('click', closePopup); 
+changeButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
